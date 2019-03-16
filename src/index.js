b5c27dadaaae7cdab4d880428a9fce83d465c8bd
mikeshukshin <merqrylive@gmail.com>
2019-03-17 01:14:13 +0300

    add form and load data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-console.log('start graph');
+import { Grapher } from './main';
+
+(function(){
+  document.addEventListener("DOMContentLoaded", ready);
+  
+  let contentElem,
+    graphElem,
+    grapher;
+  
+  function ready() {
+  
+    addListeners(data => {
+      toggleControls();
+      grapher = new Grapher(data);
+    });
+    attachElem();
+  }
+  
+  function addListeners(cb) {
+    const inputElem = document.getElementById('fileElem');
+    inputElem.addEventListener('change', onChange.bind(this, cb))
+  }
+  
+  function onChange(cb, event) {
+    var reader = new FileReader();
+    reader.onload = event => cb(JSON.parse(event.target.result));
+    reader.readAsText(event.target.files[0]);
+  }
+
+  function attachElem(){
+    contentElem = document.querySelector('.content');
+    graphElem = document.querySelector('.graph');
+  }
+  
+  function toggleControls() {
+    contentElem.classList.toggle('hidden');
+    graphElem.classList.toggle('hidden');
+  }
+})()
---
+++ src/main.js
@@ -0,0 +1,5 @@
+export class Grapher {
+  constructor(data) {
+    console.log({data});
+  }
+}
